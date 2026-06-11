51207d0ce2bc6f957b3d832513118a523931a7f0
1PJP <patrickjeam2@gmail.com>
2026-06-11 15:04:02 +0200

    update(list, dicts)

--- Python05/ex0/data_processor.py
+++ Python05/ex0/data_processor.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Tuple, Union
+#las clases astractas no llevan cuerpo, a diferencia de una normal
+#solo llevan un pass pero obliga a cada funcion llevarla y se le poner 
+#el cuerpo dentro de cada funcion
 
 class DataProcessor(ABC):
     def __init__(self) -> None:
@@ -22,7 +25,7 @@ class DataProcessor(ABC):
     
 class NumericProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
-       if isinstance(data, List):
+       if isinstance(data, list):
           return all(isinstance(i, (int, float)) for i in data)
        else:
         return isinstance(data, (int, float))
@@ -30,8 +33,41 @@ class NumericProcessor(DataProcessor):
     def ingest(self, data: Union[int, float, List[Union[int, float]]]) -> None:# o podemos usar | en ligar de Union
        if not self.validate(data):
           raise ValueError('Improper numeric data')
-       if isinstance(data, List):
-         #for i in data:
-         self._data.append(str(i) for i in data)#anadimos el elemento trasformsado en str a la Lista
+       if isinstance(data, list):
+         for i in data:
+            self._data.append(str(i))#anadimos el elemento trasformsado en str a la Lista
        else:
           self._data.append(str(data))
+
+class TextProcessor(DataProcessor):
+   def validate(self, data: Any) -> bool:
+      if isinstance(data, list):
+         return all(isinstance(i , str) for i in data)
+      else:
+         return isinstance(data, str)
+      
+   def ingest(self, data: str | List[str]) -> None:
+      if not self.validate(data):
+         raise ValueError('improper text data')
+      if isinstance(data, list):
+         for i in data:
+            self._data.append(i)
+      else:
+         self._data.append(data)
+
+class LogProcessor(DataProcessor):
+   def validate(self, data: Any) -> bool:
+      if isinstance(data, list):
+            return all(
+               isinstance(i, dict) and 
+               all(isinstance(k, str) and 
+                   isinstance(v, str) for k, v in i.items()) for 
+                   i in data
+            )
+      else:
+         valid_keys = all(isinstance(k, str) for k in data.keys())
+         valid_value = all(isinstance(v, str) for v in data.values())
+         return valid_keys and valid_value
+      #toca realizar la comprovacion para una lista de dict y para un dict solo
+      #se verifica que todas las listan sean dict validos y que cada dict tenga 
+      #valores validos, tanto clave y valor sean str
